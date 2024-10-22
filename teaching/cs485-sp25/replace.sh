cc5881401c368a554688e9e94d1c832517938e6e
Martin Kellogg <martin.kellogg@njit.edu>
2024-10-22 14:15:22 -0400

    update cs485

--- teaching/cs485-sp25/replace.sh
+++ teaching/cs485-sp25/replace.sh
@@ -1,7 +1,7 @@
 bundle exec jekyll build
-rm -rf ../martinjkellogg.com/teaching/cs490-au24
-cp -r _site ../martinjkellogg.com/teaching/cs490-au24
+rm -rf ../martinjkellogg.com/teaching/cs485-sp25
+cp -r _site ../martinjkellogg.com/teaching/cs485-sp25
 cd ../martinjkellogg.com/
-git add teaching/cs490-au24
-git commit -m "update cs490"
+git add teaching/cs485-sp25
+git commit -m "update cs485"
 git push
